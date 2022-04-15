adab1945d433262e5460909b24c840d179f4d7f6
vivianeendler <vdendler@inf.ufsm.br>
2022-04-15 19:55:34 -0300

    create schedule model

---
+++ src/model/ScheduleModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const ScheduleSchema = new mongoose.Schema(
+    {
+        scheduletTo: { type: Data, min: Date.now(), required: true },
+        name: { type: String, maxLength: 150, required: true },
+        bornDate: { type: Date, max: Date.now(), required: true },
+        email: { type: String, maxLength: 200 },
+        status: {
+            type: String,
+            enum: ["Atendido", "Não Atendido"],
+            default: "Não Atendido",
+        },
+        observation: { type: String, maxLength: 200, default: null },
+    },
+    {
+        timestamps: true,
+    },
+);
+
+const ScheduleModel = mongoose.model("schedule", ScheduleSchema);
+
+export default ScheduleModel;
